232a3c47c295800bba5052a7525e6eba9b9fbb73
Gabriel Rosa <gabriel@rosa.dev.br>
2025-07-21 11:13:17 -0300

    🎉 Base files

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "ldap-nel-docs",
+  description: "description",
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
