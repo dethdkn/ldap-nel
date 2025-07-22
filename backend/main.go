92546414cd31fe3a09c99d8af068572d93ca34f6
Gabriel Rosa <gabriel@rosa.dev.br>
2025-07-22 16:31:55 -0300

    ✨ Remove default root route handler from main server

--- backend/main.go
+++ backend/main.go
@@ -11,12 +11,6 @@ func main() {
 
 	server := gin.Default()
 
-	server.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "LDAP-NEL API",
-		})
-	})
-
 	routes.RegisterRoutes(server)
 
 	server.Run(":8080")
